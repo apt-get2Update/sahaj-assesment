8867a7114cf93770d7de60fb80ae6050232540e5
Thirumuruga <thirumurugan3131@gmail.com>
2019-11-02 12:37:48 +0530

    index changed

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const Caram = require("./Caram");
 function testcode() {
   let caram = new Caram();
   //change input compination here 
-  caram.startCaram([
+  const comments = [
     [1, 1, 2, 1, 5, 1, 1, 1, 1, 1, 6],
     [2, 3, 6, 6, 6, 7, 8, 9, 10, 3, 3]
-  ]);
+  ];
+  caram.startCaram(comments);
 }
 
 testcode();
