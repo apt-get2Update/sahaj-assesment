d55dfc2b2d426a48020ffa0ee79c39ba37563d7f
Thirumuruga <thirumurugan3131@gmail.com>
2019-11-02 12:30:11 +0530

    player test added

--- Player.js
+++ Player.js
@@ -1,33 +1,28 @@
+const FOULS_COUNT = 3; //
+const SUCCESSIVE_TURN = 3;
 export default class Player {
   point = 0;
   notPucketedTurns = 0;
   fouls = 0;
-  setPoint(point) {
-    this.point = point;
-  }
   getPoint() {
     return this.point;
-  }
-  setnotPucketedTurns(turn) {
-    this.notPucketedTurns = turn;
   }
   getnotPucketedTurns() {
     return this.notPucketedTurns;
   }
-  setFouls(num) {
-    this.fouls = num;
-  }
+
   getFouls() {
     return this.fouls;
   }
+
   hasReachedFouls() {
-    if (this.fouls === 3) {
+    if (this.fouls >= FOULS_COUNT) {
       this.point -= 1;
       this.fouls = 0;
     }
   }
   hasReachedNonPocketTurns() {
-    if (this.notPucketedTurns === 3) {
+    if (this.notPucketedTurns >= SUCCESSIVE_TURN) {
       this.point -= 1;
       this.notPucketedTurns = 0;
     }
@@ -41,18 +36,29 @@ export default class Player {
     coin.reduceBlackCoin(2);
   }
   redStrike(coin) {
-    this.point += 2;
-    coin.reduceRedCoin();
+    if (coin.getRedCoin() !== 0) {
+      this.point += 3;
+      coin.reduceRedCoin();
+    }
   }
   strikerStrike() {
-    this.point -= 1;
     this.fouls += 1;
+    if (this.point > 0) {
+      this.point -= 1;
+      this.hasReachedFouls();
+    }
   }
   defunctCoin() {
-    this.point -= 2;
     this.fouls += 1;
+    if (this.point > 0) {
+      this.point -= 2;
+      this.hasReachedFouls();
+    }
   }
   none() {
     this.notPucketedTurns += 1;
+    if (this.point > 0) {
+      this.hasReachedNonPocketTurns();
+    }
   }
 }
---
+++ player.test.js
@@ -0,0 +1,116 @@
+import Player from "./Player";
+import Coin from "./Coin";
+
+describe("Testing Player", () => {
+  describe("single strike", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.strike(coin);
+    test("wins one point", () => {
+      expect(player.getPoint()).toBe(1);
+    });
+    test("decrease one coin", () => {
+      expect(coin.getBlackCoin()).toBe(8);
+    });
+  });
+  describe("muilti strike", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.multiStrike(coin);
+
+    test("wins two point", () => {
+      expect(player.getPoint()).toBe(2);
+    });
+
+    test("decrease two coin", () => {
+      expect(coin.getBlackCoin()).toBe(7);
+    });
+  });
+
+  describe("red strike", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.redStrike(coin);
+
+    test("wins there point", () => {
+      expect(player.getPoint()).toBe(3);
+    });
+    test("decrease red coin", () => {
+      expect(coin.getRedCoin()).toBe(0);
+    });
+  });
+
+  describe("Striker strike", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.strikerStrike();
+    // when the player not get pont
+    test("if pont zero no pont loses", () => {
+      expect(player.getPoint()).toBe(0);
+    });
+    test("should increased fouls count", () => {
+      expect(player.getFouls()).toBe(1);
+    });
+
+    test("if pont not zero  pont loses", () => {
+        player.redStrike(coin); //got three points
+        player.strikerStrike();
+        expect(player.getPoint()).toBe(2);
+    });
+
+    test("should increased fouls count", () => {
+      expect(player.getFouls()).toBe(2);
+    });
+
+    test("three fouls reached reduce two points", () => {
+      player.strikerStrike();
+      expect(player.getPoint()).toBe(0);
+    });
+
+    test("after three fouls count will reset to zero", () => {
+      expect(player.getFouls()).toBe(0);
+    });
+  });
+
+  describe("Defunct Coin", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.redStrike(coin);
+    player.defunctCoin();
+
+    test("loses two ponits", () => {
+      expect(player.getPoint()).toBe(1);
+    });
+
+    test("should increased fouls count", () => {
+      expect(player.getFouls()).toBe(1);
+    }); 
+  });
+
+  describe("No pocket", () => {
+    const player = new Player();
+    const coin = Coin.getInstance();
+    player.redStrike(coin);
+    
+
+    test("fist successive turn no point lose", () => {
+        player.none();
+        expect(player.getPoint()).toBe(3);
+    });
+    test("second successive turn no point lose", () => {
+      player.none();
+      expect(player.getPoint()).toBe(3);
+    });
+    test("turn count shoul be two ", () => {
+      expect(player.getnotPucketedTurns()).toBe(2);
+    });
+    test("third successive turn should lose one point", () => {
+      player.none();
+      expect(player.getPoint()).toBe(2);
+    });
+    test("turn count shoul be zero", () => {
+      expect(player.getnotPucketedTurns()).toBe(0);
+    });
+  });
+
+});
